6a5547343c0fc4b05562516356210ac5f4314e66
Ruben Martirosyan <rubboss1992@gmail.com>
2024-10-08 22:57:52 +0400

    init: added features

---
+++ src/Components/Entry/Entry.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+function Entry(props) {
+  return (
+    <>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              {props.emoji}
+            </span>
+            <span>{props.name}</span>
+          </dt>
+          <dd>
+            {props.description}
+          </dd>
+        </div>
+    </>
+  );
+}
+
+export default Entry;
